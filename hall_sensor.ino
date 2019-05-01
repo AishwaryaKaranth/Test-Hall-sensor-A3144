a608feb3ec5b230e6b3c16a6049401d1f7f60df8
AishwaryaKaranth <35144851+AishwaryaKaranth@users.noreply.github.com>
2019-05-01 08:25:34 +0530

    Add files via upload

---
+++ hall_sensor.ino
@@ -0,0 +1,18 @@
+const int hall_sensor = 4;
+const int led = 13;
+  int hallPin=0;
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(hall_sensor, INPUT);
+
+}
+
+void loop() {
+  hallPin= digitalRead(hall_sensor);
+  if(hallPin == HIGH){
+    digitalWrite(led, LOW);
+  }
+  else{
+    digitalWrite(led, HIGH);
+  }
+}
